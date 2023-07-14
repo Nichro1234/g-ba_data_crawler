2427ba793b20dd16ee265354afdad928a1d222a3
wei <2>
2023-07-14 07:35:28 +0100

    Added table_info_extractor.py, which provides parsing for the tables
    Added test code to pdf_parser.py

--- src/pdf_parser.py
+++ src/pdf_parser.py
@@ -17,7 +17,8 @@ config.read("config.ini", encoding="utf-8")
 def convert_pdf(pdf_path):
     cv = Converter(pdf_path)
     temp_path = config.get("pdf_parser", "temp_path")
-    docx_path = os.path.join(temp_path, pdf_path + '.docx')
+    docx_path = os.path.join(temp_path, pdf_path.split("\\")[-1] + '.docx')
+    print(docx_path)
 
     cv.convert(docx_path, start=0, end=None)
     cv.close()
@@ -66,15 +67,58 @@ def extract_tables(docx_path):
 
     assessment_detail_tables = {}
 
-    # Show only the tables related to the
+    # Show only the tables related to the four measures
     for title, block in iter_block_items(doc):
         if title in ["Mortalität ", "Morbidität ", "Gesundheitsbezogene Lebensqualität ", "Nebenwirkungen "]:
             if ('Endpunkt ', 'Endpunkt ') in block[0].items():
-                assessment_detail_tables[title] = block
+
+                # Not a good solution. Currently, we assume that only the table heads are going to be the same for
+                # tables that crosses pages, which is not the case
+                assessment_detail_tables[title] = utils.list_remove_duplicate(block)
     return assessment_detail_tables
 
 
 def init():
+    temp_storage_path = config.get("pdf_parser", "temp_path")
+    utils.check_create_path(temp_storage_path, path_of="docx")
+
+
+# Test Code
+if __name__ == '__main__':
+    init()
+
     pdf_storage_path = config.get("pdf_crawler", "pdf_storage_path")
-    utils.check_create_path(pdf_storage_path, path_of="pdf")
+    pdf_list = os.listdir(pdf_storage_path)
+    pdf_list = [i for i in pdf_list if i.endswith(".pdf")]
+    import table_info_extractor
+
+    for pdf_file in pdf_list:
+        docx_path = convert_pdf(os.path.join(pdf_storage_path, pdf_file))
+        tables = extract_tables(docx_path)
+
+        result = {}
+        for endpoint_category, table in tables.items():
+            category_table = table_info_extractor.derive_metric_names(table)
+            cleaned_table = table_info_extractor.clean_up_table(category_table)
+
+            result[endpoint_category] = cleaned_table
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/table_info_extractor.py
+++ src/table_info_extractor.py
@@ -1,33 +1,61 @@
 import re
+import copy
 
 
-def extract_data_from_table(table):
-    for row in table:
+def derive_metric_names(table):
+    _table = copy.deepcopy(table)
+    for i, row in enumerate(_table):
+        if len(set(row.values())) == 1:
+            _table[i] = next(iter(set(row.values())))
+    return _table
+
+
+def clean_up_table(table):
+    prev_text = "table_head"
+
+    result = {}
+    table_buffer = []
+
+    for i, row in enumerate(table):
+        if type(row) == str and table_buffer:
+            result[prev_text] = table_buffer
+            prev_text = row
+            table_buffer = []
+
+        else:
+            table_buffer.append(row)
+    result[prev_text] = table_buffer
+    return result
+
+
+def process_endpoint_result_str(result_str):
+    _result = re.split(r"\n?[\(\[].*[\)\]]", result_str)
+    num_and_space_only = [re.sub(r"[^0-9, ]", "", i) for i in _result]
+    num_only = []
+    for i in num_and_space_only:
+        num_only += i.split(" ")
+    return [i for i in num_only if i != ""]
+
+
+def get_endpoint_data(table, endpoint):
+    if endpoint in table.keys():
+        endpoint_data = table[endpoint]
+        return crawl_multi_study_assessment(endpoint_data)
+
+    else:
+        for sub_table in table.values():
+            for row in sub_table:
+                if row["Endpunkt "] == endpoint:
+                    return process_endpoint_result_str(row["Intervention vs. Kontrolle"])
+    return "Data not Found"
+
+
+def crawl_multi_study_assessment(endpoint_data):
+    for row in endpoint_data:
         if row['Endpunkt '] == "" or row['Endpunkt '] == "Metaanalyse":
             target = row["Intervention vs. Kontrolle"]
-            list_of_data = re.split(r"\s", target)
-            list_of_data = [i for i in list_of_data if i != ""]
-            return list_of_data[0], list_of_data[-1]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            list_of_data = process_endpoint_result_str(target)
+            return list_of_data[0], list_of_data[1]
+    target = endpoint_data[-1]["Intervention vs. Kontrolle"]
+    list_of_data = process_endpoint_result_str(target)
+    return list_of_data[0], list_of_data[1]
